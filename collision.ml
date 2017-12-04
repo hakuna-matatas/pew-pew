5052ccc1b6b72bc528e115c6938add5bcf15a495
Newton Ni <nwtnni@gmail.com>
2017-12-04 16:48:34 -0500

    Refactoring collision data structure to record collisions as objects are added

--- collision.ml
+++ collision.ml
@@ -21,10 +21,6 @@ let to_pos = function
 | Player (_, _, pos)
 | Rock   (_, _, pos) -> pos
 
-let solid = function
-| Ammo   _ | Gun _              -> false
-| Bullet _ | Player _ | Rock  _ -> true
-
 let to_cell e =
   let bins = 5000.00 in
   let x, y = to_pos e in 
@@ -40,6 +36,7 @@ type map  = (id, State.entity) Hashtbl.t
 type t = {
   grid : grid;
   prev : map;
+  coll : (State.entity * State.entity) list
 }
 
 (* Does not preserve order *)
@@ -54,25 +51,26 @@ let bin_remove b e =
 (* Does not preserve order of list [l] *)
 let bin_replace b e = e :: (bin_remove b e)
 
+let add g e = 
+  let c = to_cell e in
+  let _ = Hashtbl.replace g.prev (to_id e) e in
+  if Hashtbl.mem g.grid c then
+    let b  = Hashtbl.find g.grid c in
+    let b' = bin_replace b e in
+    Hashtbl.replace g.grid c b'
+  else
+    Hashtbl.add g.grid c [e]
+
 let create s =
-  let g = Hashtbl.create 16 in
-  let l = s |> State.to_list |> List.filter solid in
+  let g = {grid = Hashtbl.create 16; prev = Hashtbl.create 16; coll = []} in
+  let l = s |> State.to_list in
   let rec create' = function
   | []     -> ()
-  | h :: t -> 
-    let c = to_cell h in
-    let _ = if Hashtbl.mem g c then
-      let b  = Hashtbl.find g c in
-      let b' = bin_replace b h in
-      Hashtbl.replace g c b'
-    else
-      Hashtbl.add g c [h] in
-    create' t in
+  | h :: t -> add g h; create' t in
   create' l
 
 let remove g e = 
   let id = to_id e in
-  if not (solid e) then () else
   if not (Hashtbl.mem g.prev id) then () else
   let c  = Hashtbl.find g.prev id |> to_cell in
   let b  = Hashtbl.find g.grid c  in
@@ -81,18 +79,15 @@ let remove g e =
   if b' = [] then Hashtbl.remove g.grid c 
   else Hashtbl.replace g.grid c b'
 
-let update g e =
-  let _ = remove g e in
-  let _ = Hashtbl.add g.prev (to_id e) e in
-  let c = to_cell e in
-  if Hashtbl.mem g.grid c then
-    let b = Hashtbl.find g.grid c in
-    Hashtbl.replace g.grid c (e :: b)
-  else
-    Hashtbl.add g.grid c [e]
+let update g e = remove g e; add g e
 
-let around (x, y) = [
-  (x-1, y+1); (x, y+1); (x+1, y+1)
-  (x-1, y);             (x+1, y);
-  (x-1, y-1); (x, y-1); (x+1, y-1)
-]
+let sqdist (x1, y1) (x2, y2) =
+  let x = x2 -. x1 in
+  let y = y2 -. y1 in
+  (x*.x) +. (y*.y)
+
+let collision e1 e2 =
+  let r = (to_rad e1) +. (to_rad e2) in
+  let rs = r*.r in
+  sqdist (to_pos e1) (to_pos e2) <= rs
+
