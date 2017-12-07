83192437e4b00f7ee10b896081d13dbfaa4c5ba2
Talha Baig <tbaig6@gmail.com>
2017-12-07 06:40:43 -0500

    added draw.mli

---
+++ draw.mli
@@ -0,0 +1,4 @@
+(* [draw_state st pid gun] draws the state of the world
+    as represented by [st]. [pid] and [gun] is the player id
+    and gun name, respectively. *)
+val draw_state: Ctype.state -> Ctype.id -> Ctype.name -> unit
