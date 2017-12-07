570beee555e0054649ee9e66d09272a7f9941927
Alan Gao <alangao50@gmail.com>
2017-12-07 02:19:12 -0500

    Main loop uses lobby ref now

--- main.ml
+++ main.ml
@@ -1,15 +1,15 @@
-open State
-open Server
-
 let delay = 0.033
 
 let sleep sec = 
 	ignore (Unix.select [] [] [] sec)
 
-let rec step lobby = 
-	sleep delay; step lobby
+let rec step () = 
+	let states = List.map (fun (gid,st) -> st) !(Server.get_lobby ()) in
+	sleep delay; 
+	List.iter State.step states;
+	step ()
 
 let main () = 
-	let _ = Thread.create run 8000 in step []
+	let _ = Thread.create Server.run 8000 in step ()
 
 let () = main ()
