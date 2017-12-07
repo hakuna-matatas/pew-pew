08ac03e80f0d0aedbb5b395c8e15be7215d74b85
Newton Ni <nwtnni@gmail.com>
2017-12-07 08:11:13 -0500

    Text-based lobby implementation

--- lobby.ml
+++ lobby.ml
@@ -1,45 +1,76 @@
-open GMain
-open GdkKeysyms
-open GToolbox
-open GdkEvent
+open Ctype
+open Settings
 
-let locale = GtkMain.Main.init ()
+let p_name = ref ""
+let p_id   = ref (-1)
+let g_id   = ref (-1)
 
-let try_user= input_string "Username" ""
-let get_username= match try_user with
-  | None-> "Bob"
-  | Some a-> (match a with
-      | ""-> "No name"
-      | a-> a)
+let string_of_players p = let s = p
+  |> List.fold_left (fun acc p -> acc ^ p ^ ", ") "" in
+  String.sub s 0 (String.length s - 2)
 
-let main () =
+let print_description d =
+  print_newline ();
+  print_endline ("Name   : " ^ d.game_name);
+  print_endline ("ID     : " ^ (string_of_int d.game_id));
+  print_endline ("Players: " ^ string_of_players d.game_players);
+  print_newline ()
 
-  let window = GWindow.window ~width:300 ~height:300
-      ~title:"Apex" () in
-  let vbox = GPack.vbox ~packing:window#add () in
-  window#connect#destroy ~callback:Main.quit;
+let print_help () =
+  print_newline ();
+  print_endline ("Welcome to the Apex lobby!");
+  print_endline ("Type 'games' to view a list of all available games.");
+  print_endline ("Type 'create <GAME_NAME>' to create a new game with name <GAME_NAME>.");
+  print_endline ("Type 'join <GAME_ID>' to join the game with ID <GAME_ID>.");
+  print_endline ("Type 'help' to bring up this information again.");
+  print_newline ()
 
-  (* Menu bar *)
-  let menubar = GMenu.menu_bar ~packing:vbox#pack () in
-  let factory = new GMenu.factory menubar in
-  let accel_group = factory#accel_group in
-  let file_menu = factory#add_submenu "File" in
-  let username= get_username in
-  let make a =
-  (* Button *)
-    (GButton.button ~label:(string_of_int a)
-       (*fun () shoudl make a router call to join a lobby with id [a]*)
-       ~packing:vbox#add ())#connect#clicked ~callback: (fun () -> prerr_endline (("Join lobby ")^(string_of_int a)^username)); in
-  (* lobby list is list of currently open lobbies*)
-  let lobby_list = [24; 34; 45] in
-  List.map make lobby_list;
-  let openbutton = GButton.button ~label:"Create new lobby."
-                              ~packing:vbox#add () in
-  openbutton#connect#clicked ~callback: (fun () -> prerr_endline ("Create new lobby."^username));
+let prompt () =
+  print_newline ();
+  print_endline ("Welcome to Apex! Please enter a username below.");
+  let s = read_line () in
+  print_newline (); s
 
-  (* Display the windows and enter Gtk+ main loop *)
-  window#add_accel_group accel_group;
-  window#show ();
-  Main.main ()
+let lobbies () = Router.get_lobbies (fun l -> l)
 
-let () = main ()
+type command =
+| List
+| Join of id
+| Create of name
+
+let parse s = match Str.split (Str.regexp " ") s with
+| "games"  :: []         -> Some List
+| "create" :: name :: [] -> Some (Create name)
+| "join"   :: id   :: [] -> 
+  begin match int_of_string_opt id with
+  | Some n -> Some (Join n)
+  | None   -> None end
+| _ -> None
+
+let eval = function
+| List   -> List.iter print_description (lobbies ())
+| Create name -> 
+  begin
+    let _ = Router.create_lobby name !p_name 
+    (fun r -> p_id := r.rplayer_id; g_id := r.rgame_id) in ()
+  end
+| Join n -> 
+  begin 
+    if List.exists (fun l -> l.game_id = n) (lobbies ()) then
+      let _ = Router.join_lobby n !p_name (fun r -> p_id := r; g_id := n) in ()
+    else ()
+  end
+
+let rec lobby () = match read_line () |> parse with
+| None -> print_newline (); print_endline "Invalid command."; print_newline (); lobby ()
+| Some c -> let _ = eval c in
+  if (!p_id <> (-1)) && (!g_id <> (-1)) then 
+    let _ = Client.create !g_id !p_id |> Client.run in ()
+  else
+    print_newline (); print_endline "Invalid game ID."; print_newline (); lobby ()
+  
+let rec init () =
+  if !p_name = "" then let _ = p_name := prompt () in init ()
+  else print_help (); lobby ()
+
+let () = init ()
